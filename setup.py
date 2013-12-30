f4b5991281745057901cf5d81845a8a9dc3f7e36
kAlmAcetA <kalmaceta@2147483647.net>
2013-12-30 16:43:39 +0100

    setup.py file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name="vboxtray",
+    packages=['vboxtray'],
+    version="0.1.5",
+    author="kwarunek",
+    author_email="kalmaceta@gmail.com",
+    description="Virtualbox tray tool - list/start/stop VMs",
+    license="BSD",
+    keywords="virtualbox tray pysidel",
+    url="https://github.com/kAlmAcetA/vboxtray",
+    long_description='Virtualbox icon tray. Quick access start/stop VMs',
+    entry_points={
+        'console_scripts': ['vboxtray = vboxtray.core:main']
+    },
+)
