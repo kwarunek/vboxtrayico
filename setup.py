72ef17583265a8f55e7c6903e1d87e18b54c08b2
kwarunek <kAlmAcetA@users.noreply.github.com>
2013-12-30 18:10:59 +0100

    tune setup

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
-from setuptools import setup
+#!/usr/bin/env python
 
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name="vboxtray",
     packages=['vboxtray'],
     version="0.1.5",
-    author="kwarunek",
+    author="Krzysztof Warunek",
     author_email="kalmaceta@gmail.com",
     description="Virtualbox tray tool - list/start/stop VMs",
     license="MIT",
-    keywords="virtualbox tray pysidel",
+    keywords="virtualbox tray vm pyside",
     url="https://github.com/kAlmAcetA/vboxtray",
     long_description='Virtualbox icon tray. Quick access start/stop VMs',
     entry_points={
