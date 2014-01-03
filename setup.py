de74c4dc70822d2440548874d7c0815dc469226f
kAlmAcetA <kalmaceta@2147483647.net>
2014-01-03 21:17:49 +0100

    setup classifiers

--- setup.py
+++ setup.py
@@ -16,5 +16,14 @@ setup(
     keywords="virtualbox tray vm pyside",
     url="https://github.com/kAlmAcetA/vboxtrayico",
     long_description='Virtualbox icon tray. Quick access start/stop VMs',
-    scripts=['scripts/vboxtrayico']
+    scripts=['scripts/vboxtrayico'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Environment :: X11 Applications :: Qt',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Development Status :: 3 - Alpha'
+    ]
 )
